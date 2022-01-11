7a243b6992f29cf0f11836b0c9c8214ede7a2667
jianghaiqiang <jhq0113@163.com>
2022-01-11 14:04:38 +0800

    init

--- deserializer.go
+++ deserializer.go
@@ -4,12 +4,29 @@ import (
 	"github.com/grpc-boot/base"
 )
 
+const (
+	String = "string"
+	Json   = "json"
+	Yaml   = "Yaml"
+)
+
+// KeyOption Key配置
+type KeyOption struct {
+	Key         string `json:"key" yaml:"key"`
+	Type        string `json:"type" yaml:"type"`
+	Deserialize Deserialize
+}
+
+// Deserialize 反序列化函数
 type Deserialize func(data []byte) (value interface{}, err error)
 
+// Deserializer 反序列化工具
 type Deserializer interface {
+	// Deserialize 反序列化
 	Deserialize(key string, val []byte) (value interface{}, err error)
 }
 
+// NewDeserializer 实例化反序列化工具对象
 func NewDeserializer(options map[string]KeyOption) Deserializer {
 	return &deserializer{keyOptions: options}
 }
@@ -42,18 +59,21 @@ func (d *deserializer) Deserialize(key string, val []byte) (value interface{}, e
 	return val, err
 }
 
+// JsonMapDeserialize Json反序列化为map[string]interface{}
 func JsonMapDeserialize(data []byte) (value interface{}, err error) {
 	var val map[string]interface{}
 	err = base.JsonDecode(data, &val)
 	return val, err
 }
 
+// YamlMapDeserialize Yaml反序列化为map[string]interface{}
 func YamlMapDeserialize(data []byte) (value interface{}, err error) {
 	var val map[string]interface{}
 	err = base.YamlDecode(data, &val)
 	return val, err
 }
 
+// StringDeserialize 字符串
 func StringDeserialize(data []byte) (value interface{}, err error) {
 	return base.Bytes2String(data), nil
 }
